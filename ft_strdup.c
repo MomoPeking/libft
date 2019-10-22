7f309f1a35e0796264534d3906fbc78e4b347f1f
qdang <dqzz369@gmail.com>
2019-10-21 17:19:28 -0700

    modified for get_next_line

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: qdang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 13:22:15 by qdang             #+#    #+#             */
-/*   Updated: 2019/10/19 15:16:34 by qdang            ###   ########.fr       */
+/*   Updated: 2019/10/21 16:13:15 by qdang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s1)
 	char	*str;
 
 	i = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	str = ft_strnew(ft_strlen(s1));
 	if (!str || !s1)
 		return (NULL);
 	while (s1[i])
@@ -27,6 +27,5 @@ char	*ft_strdup(const char *s1)
 		str[i] = s1[i];
 		i++;
 	}
-	str[i] = '\0';
 	return (str);
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: qdang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/20 20:14:44 by qdang             #+#    #+#             */
-/*   Updated: 2019/09/26 17:03:54 by qdang            ###   ########.fr       */
+/*   Updated: 2019/10/21 16:15:59 by qdang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	size;
 	size_t	i;
 	size_t	j;
 	char	*str;
@@ -23,20 +22,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	j = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	size = ft_strlen(s1) + ft_strlen(s2);
-	str = ft_strnew(size);
+	str = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
 	if (!str)
 		return (NULL);
-	while (i < ft_strlen(s1))
+	while (s1[i])
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	while (j < ft_strlen(s2))
+	while (s2[j])
 	{
 		str[i + j] = s2[j];
 		j++;
 	}
-	str[i + j] = '\0';
 	return (str);
 }
